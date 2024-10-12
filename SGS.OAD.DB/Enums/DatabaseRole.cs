82e7cf207e0a5ba07dc1e3dc26c5005053d09a0d
brian_li <brian.li@sgs.com>
2024-10-12 14:39:27 +0800

    comment Enum

--- SGS.OAD.DB/Enums/DatabaseRole.cs
+++ SGS.OAD.DB/Enums/DatabaseRole.cs
@@ -1,9 +1,23 @@
 ﻿namespace SGS.OAD.DB.Enums
 {
+    /// <summary>
+    /// 資料庫角色
+    /// </summary>
     public enum DatabaseRole
     {
+        /// <summary>
+        /// 具備讀寫權限者
+        /// </summary>
         db_datawriter = 0,
+
+        /// <summary>
+        /// 檔案IO權限角色(非資料庫)
+        /// </summary>
         db_filewriter = 1,
+
+        /// <summary>
+        /// 具備完整權限者
+        /// </summary>
         db_owner = 2
     }
 }
--- SGS.OAD.DB/Enums/ProgramLanguage.cs
+++ SGS.OAD.DB/Enums/ProgramLanguage.cs
@@ -1,9 +1,23 @@
 ﻿namespace SGS.OAD.DB.Enums
 {
+    /// <summary>
+    /// 程式語言種類
+    /// 依照 TIOBE Index 2024/10 排名
+    /// https://www.tiobe.com/tiobe-index/
+    /// </summary>
     public enum ProgramLanguage
     {
+        /// <summary>
+        /// C# 排名第 5
+        /// </summary>
         Csharp = 0,
+        /// <summary>
+        /// Delphi/Object Pascal 排名第 10
+        /// </summary>
         Delphi = 1,
+        /// <summary>
+        /// Delphi/Object Pascal 排名第 10
+        /// </summary>
         DelphiXE = 2
     }
 }
