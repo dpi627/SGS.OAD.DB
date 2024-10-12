c391da0a3027e7dbeb58a3c11eef46f832d501b8
brian_li <brian.li@sgs.com>
2024-10-12 14:29:34 +0800

    comment Util

--- SGS.OAD.DB/Utilities/Util.cs
+++ SGS.OAD.DB/Utilities/Util.cs
@@ -4,57 +4,73 @@ namespace SGS.OAD.DB.Utilities
 {
     public class Util
     {
+        /// <summary>
+        /// Replace variables in the template with the values in the object[].
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static string ReplaceVariables(string template, params object[] parameters)
         {
-            // 使用正则表达式匹配 {变量名称} 的模式
             string pattern = @"\{(\w+)\}";
 
-            // 提取模板中的变量名列表，按出现顺序排列
+            // 取出樣板中所有 {varb}
             var variableNames = Regex.Matches(template, pattern)
                 .Cast<Match>()
                 .Select(match => match.Groups[1].Value)
                 .ToList();
 
-            // 检查参数数量是否匹配
+            // 檢查傳入參數是否足夠
             if (parameters.Length < variableNames.Count)
             {
-                throw new ArgumentException("提供的参数数量少于模板中需要替换的变量数量。");
+                throw new ArgumentException("parameters are less than {variables}");
             }
 
-            // 构建变量名与参数值的字典
+            // convert parameters to dictionary
             var variables = new Dictionary<string, object>();
             for (int i = 0; i < variableNames.Count; i++)
             {
                 variables[variableNames[i]] = parameters[i];
             }
 
-            // 使用字典进行变量替换
             return ReplaceVariables(template, variables);
         }
+
+        /// <summary>
+        /// Replace variables in the template with the values in the tuple[].
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
         public static string ReplaceVariables(string template, params (string Name, object Value)[] parameters)
         {
             var variables = parameters.ToDictionary(p => p.Name, p => p.Value);
             return ReplaceVariables(template, variables);
         }
+
+        /// <summary>
+        /// Replace variables in the template with the values in the dictionary.
+        /// </summary>
+        /// <param name="template">string template</param>
+        /// <param name="variables"></param>
+        /// <returns></returns>
         public static string ReplaceVariables(string template, Dictionary<string, object> variables)
         {
-            // 使用正则表达式匹配 {变量名称} 的模式
             string pattern = @"\{(\w+)\}";
 
-            // 使用 Regex.Replace 逐一替换 {变量}
+            // 替換 {varb}
             string result = Regex.Replace(template, pattern, match =>
             {
                 string key = match.Groups[1].Value;
+
                 if (variables.ContainsKey(key))
                 {
                     string replacement = variables[key]?.ToString() ?? string.Empty;
                     return replacement;
                 }
-                else
-                {
-                    // 如果变量未提供，保留原始占位符
-                    return match.Value;
-                }
+
+                return match.Value;
             });
 
             return result;
